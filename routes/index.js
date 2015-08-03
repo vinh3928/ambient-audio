572f96d39943aebadb4ec103071f362b1b9b6059
Vinh Nguyen <vnguyen.cheme@gmail.com>
2015-08-03 09:22:26 -0600

    added env variables

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
+require('dotenv').load();
 var express = require('express');
 var router = express.Router();
 var M2X = require("m2x-tessel");
-var m2xClient = new M2X("ef4642f74095db38bb8e4fe0598cbd07");
+var m2xClient = new M2X(process.env.M2X_API_KEY);
 var streams = {
     "sound": {
         "unit": {
@@ -34,29 +35,11 @@ router.get('/tessel', function(req, res, next) {
 });
 
 router.post('/tessel', function(req, res, next) {
-  m2xClient.devices.updateStreams("4690ce92affcda9190579d380675dc6d", streams, function(response) {
+  m2xClient.devices.updateStreams(process.env.M2X_DEVICE_KEY, streams, function(response) {
       if (response.isError()) {
           console.log("Cannot create stream:", response);
           return;
       }
-
-      //var value_sound = 10;
-      //var value_light = 15;
-      //console.log("I'm updating stream values! (Press CTRL + C to stop)");
-      //var at = new Date().toISOString();
-      //var values = {
-          //sound:  [ { value: value_sound, timestamp: at } ],
-          //light:  [ { value: value_light, timestamp: at } ],
-      //};
-
-      //// Write the different values into AT&T M2X
-      //m2xClient.devices.postMultiple("4690ce92affcda9190579d380675dc6d", values, function(result) {
-          //if (result.isError()) {
-              //clearInterval(handleLoop);
-              //console.log(result.error());
-          //}
-      //});
-  //});
 
     var value_sound = +req.body.sound;
     var value_light = +req.body.light;
@@ -70,7 +53,7 @@ router.post('/tessel', function(req, res, next) {
         };
 
         // Write the different values into AT&T M2X
-        m2xClient.devices.postMultiple("4690ce92affcda9190579d380675dc6d", values, function(result) {
+        m2xClient.devices.postMultiple(process.env.M2X_DEVICE_KEY, values, function(result) {
             if (result.isError()) {
                 clearInterval(handleLoop);
                 console.log(result.error());
